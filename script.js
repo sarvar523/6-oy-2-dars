95e0bdef5fe9530bf86d5e948796a7dc7b416649
sarvar523 <sarvarakmalovich523@gmail.com>
2025-08-14 22:47:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let a = ()=>{
+    let set = prompt ('nechta')
+    let con = confirm(set * 45000+'som \n zakaz qilasizmi')
+    switch(con){
+        case true:
+            alert('qabul qilindi')
+            break
+            default:
+                alert(' otkaz qilindi')
+    }
+}
+
+                    let b = ()=>{
+                        let set1 = prompt ('nechta')
+                            let con2 = confirm(set1 * 79000+'som \n zakaz qilasizmi')
+                                switch(con2){
+                                     case true:
+                                        alert('qabul qilindi')
+                                            break
+                                                default:
+                                                    alert(' otkaz qilindi')
+    }
+}
+let c = ()=>{
+    let set = prompt ('nechta')
+    let con = confirm(set * 34000+'som \n zakaz qilasizmi')
+    switch(con){
+        case true:
+            alert('qabul qilindi')
+            break
+            default:
+                alert(' otkaz qilindi')
+    }
+}
